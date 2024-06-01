e201a82e2c774dff3d51b03fe788db7bbdd5df73
neogoolboss <157683242+neogoolboss@users.noreply.github.com>
2024-06-01 20:59:48 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) => {
+    a.src = 'https://i.namu.wiki/i/DIWQPMFg_xE7JxIv0-4M5PbXco2d-BynsivSWqt6enqDgXOKw0nuZznBUGV-7FtJilQEY7zxodg1kZcYlQXDJw.webp'
+})
+
+}, 500)
